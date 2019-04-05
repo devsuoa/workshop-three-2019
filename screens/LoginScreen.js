2ffbf7f961dd8a946fb1989a07724837d71480ad
Ben <bennypayne12@gmail.com>
2019-04-05 17:02:48 +1300

    add login screen boilerplate

---
+++ screens/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export class LoginScreen extends React.Component {
+    render() {
+        return (
+            <View style={{flex: 1, backgroundColor: "blue"}}>
+                <View style={{backgroundColor: "white", flex: 1}}>
+                    <Text>Hello from the header area </Text>
+                </View> 
+                <View style={{backgroundColor: "black", flex: 1}}>
+                    <Text>Hello from the form area </Text>
+                </View>
+
+            </View>
+        )
+    }
+}
+// 1/ (1 + 1) = 50%
