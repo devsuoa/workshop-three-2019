4ee4e325d306da4f8b76eb01eb050d4f1deddc7e
Ben <bennypayne12@gmail.com>
2019-04-05 17:08:52 +1300

    add styles

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,10 +5,10 @@ export class LoginScreen extends React.Component {
     render() {
         return (
             <View style={{flex: 1, backgroundColor: "blue"}}>
-                <View style={{backgroundColor: "white", flex: 1}}>
+                <View style={{backgroundColor: "white", flex: 2}}>
                     <Text>Hello from the header area </Text>
                 </View> 
-                <View style={{backgroundColor: "black", flex: 1}}>
+                <View style={{backgroundColor: "black", flex: 3}}>
                     <Text>Hello from the form area </Text>
                 </View>
 
