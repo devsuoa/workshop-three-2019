a7b2f4ec2574f8de3232e02fba4de2c848f08103
Andrew Hu <andrew.h318@hotmail.co.nz>
2019-05-19 13:12:00 +1200

    add boilerplate screens

---
+++ screens/MapScreen/MapScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export class MapScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello from the map screen</Text>
+            </View>
+        )
+    }
+}
---
+++ screens/SwipeScreen/SwipeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export class SwipeScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello from the swipe screen</Text>
+            </View>
+        )
+    }
+}
