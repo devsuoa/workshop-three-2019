be277e19bc1d0c4493ae678b12eb20288df85d2f
Andrew Hu <andrew.h318@hotmail.co.nz>
2019-05-19 12:51:23 +1200

    create login screen folder

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { LoginScreen } from "./screens/LoginScreen";
+import { LoginScreen } from "./screens/LoginScreen/LoginScreen";
 export default class App extends React.Component {
   render() {
     return (
--- screens/LoginScreen/LoginScreen.js
+++ screens/LoginScreen/LoginScreen.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity, TextInput, KeyboardAvoidingView } from "react-native";
 import styles from "./LoginScreen.styles";
-import { MaterialButton } from "../components/MaterialButton/MaterialButton";
+import { MaterialButton } from "../../components/MaterialButton/MaterialButton";
 
 export class LoginScreen extends React.Component {
     render() {
         return (
             <KeyboardAvoidingView behavior="padding" style={styles.loginContainer}>
                 <View style={styles.headerContainer}>
-                    <Image style={styles.imageStyle} source={require("../assets/logo.png")}/>
+                    <Image style={styles.imageStyle} source={require("../../assets/logo.png")}/>
                     <Text style={styles.headerText}>Your next job is only a Swipe away</Text>
                 </View> 
                 <View style={styles.formContainer}>
