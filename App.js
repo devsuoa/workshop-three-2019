bc977a117ad8d17cb2ae3ff60113fff891903f1c
David Ye <andrew.h318@hotmail.co.nz>
2019-05-24 17:55:48 +1200

    add navigation

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LoginScreen } from "./screens/LoginScreen/LoginScreen";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { MapScreen } from "./screens/MapScreen/MapScreen";
+import { ReviewScreen } from "./screens/ReviewScreen/ReviewScreen";
+import { SwipeScreen } from "./screens/SwipeScreen/SwipeScreen"
+
+const TabNavigator = createBottomTabNavigator({
+  map: MapScreen,
+  review: ReviewScreen,
+  swipe: SwipeScreen
+})
+
+const AppContainer = createAppContainer(TabNavigator);
+
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <LoginScreen />
+      <AppContainer />
     );
   }
 }
